8ec4c364c48fa892f8d19e06be1e91890100fff2
Matheus-Cahu <matheus.cahu@unifesp.br>
2026-01-19 09:15:26 -0300

    Add Resistência
    Como há mais RTPs de Resistência que de QDischarge, a ideia é estimar as
    proporções entre elas para estimar as QDischarges nas demais RTPs

--- RPT.py
+++ RPT.py
@@ -1,17 +1,28 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Importação dos datasets
 rpt = pd.read_csv('datasets/VAH01_impedance.csv')
+rpt = rpt.rename(columns={'cycle numbers': 'cycleNumber'})
 df = pd.read_csv('datasets/VAH01.csv')
 
-ciclos = rpt['cycle numbers'].tolist()
+# Declaração da tabela de filtros a partir de ciclos de RTP
+ciclos = rpt['cycleNumber'].tolist()
 print(f"CICLOS:\nN° de elementos: {len(ciclos)} \n {ciclos}")
+
+# Filtra VAH a partir dos ciclos de RTP
 df_filtrando = df[df['cycleNumber'].isin(ciclos)]
 df_filtrado = df_filtrando.drop_duplicates(subset='cycleNumber', keep='last')
 
-df_discharge = df_filtrado[['cycleNumber','QDischarge_mA_h']].sort_values('cycleNumber')
-print(f"DF_DISCHARGE: \nN° de elementos: {len(df_discharge)} \n {df_discharge}")
+# Busca Resistências RTP
+df_discharge = pd.merge(df_filtrado[['cycleNumber','QDischarge_mA_h']].sort_values('cycleNumber'), rpt[['cycleNumber','60%_30_second']], on='cycleNumber', how='inner')
 
+# Calcula proporção entre R e QD para estimar as QD em demais RTPs
+df_discharge['proporcao'] = df_discharge['QDischarge_mA_h'] / df_discharge['60%_30_second']
+
+print(f"DF_DISCHARGE: \nN° de elementos: {len(df_discharge)} \n {df_discharge} \n {df_discharge['QDischarge_mA_h']/df_discharge['60%_30_second']}")
+
+# Traça gráfico
 plt.figure(figsize=(10, 6))
 plt.plot(df_discharge['cycleNumber'], df_discharge['QDischarge_mA_h'], marker='o', linestyle='-', color='b', label='Capacidade de descarga')
 
@@ -20,8 +31,6 @@ plt.xlabel('Número do ciclo', fontsize=12)
 plt.ylabel('Capacidade de descarga', fontsize=12)
 plt.grid(True, linestyle='--', alpha=0.7)
 
-# Adiciona a legenda
 plt.legend()
 
-# Exibe o gráfico na tela
 plt.show()
